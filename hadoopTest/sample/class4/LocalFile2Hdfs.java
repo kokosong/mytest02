984a40ad8410f96897096d29f3fc794c2f490c33
song <kokosong1989@163.com>
2018-01-26 07:30:43 +0000

    test201801261630

---
+++ hadoopTest/sample/class4/LocalFile2Hdfs.java
@@ -0,0 +1,46 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.net.URI;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+import org.apache.hadoop.util.Progressable;
+
+public class LocalFile2Hdfs {
+    public static void main(String[] args) throws Exception {
+
+		String local = args[0];
+		String uri = args[1];
+		
+		FileInputStream in = null;
+		OutputStream out = null;
+		Configuration conf = new Configuration();
+		try {
+			in = new FileInputStream(new File(local));
+
+			FileSystem fs = FileSystem.get(URI.create(uri), conf);
+			out = fs.create(new Path(uri), new Progressable() {
+				@Override
+				public void progress() {
+					System.out.println("*");
+				}
+			});
+
+			in.skip(100);
+			byte[] buffer = new byte[20];
+				
+			int bytesRead = in.read(buffer);
+			if (bytesRead >= 0) {
+				out.write(buffer, 0, bytesRead);
+			}
+		} finally {
+			IOUtils.closeStream(in);
+			IOUtils.closeStream(out);
+		}		
+	}
+}
